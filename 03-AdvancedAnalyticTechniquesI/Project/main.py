647989118a361502cda2c449272b94c9552d74f4
YeJiu97 <yejiuwork@outlook.com>
2023-10-30 22:18:52 +1030

    time_series_report_marked

---
+++ 03-AdvancedAnalyticTechniquesI/Project/main.py
@@ -0,0 +1,70 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.neural_network import MLPClassifier
+
+# 数据
+A = [0, 0, 1, 1]
+B = [0, 1, 0, 1]
+XOR = [0, 1, 1, 0]
+
+# 分别提取XOR为0和1的数据点
+x0, y0, x1, y1 = [], [], [], []
+for i in range(len(XOR)):
+    if XOR[i] == 0:
+        x0.append(A[i])
+        y0.append(B[i])
+    else:
+        x1.append(A[i])
+        y1.append(B[i])
+
+# 设置画布大小和范围
+plt.figure(figsize=(6, 4))
+plt.xlim(-0.5, 1.5)
+plt.ylim(-0.5, 1.5)
+
+# 绘制XOR数据的可视化
+plt.scatter(x0, y0, c='red', label='XOR=0', marker='o', s=100)
+plt.scatter(x1, y1, c='blue', label='XOR=1', marker='x', s=100)
+plt.xlabel('A')
+plt.ylabel('B')
+plt.title('XOR Data Visualization')
+plt.legend(loc='upper right')
+plt.show()
+
+# 绘制传统感知器的可视化
+plt.figure(figsize=(6, 4))
+plt.xlim(-0.5, 1.5)
+plt.ylim(-0.5, 1.5)
+x_boundary = np.linspace(-0.5, 1.5, 100)
+y_boundary = -x_boundary + 0.5  # 感知器的线性决策边界
+plt.plot(x_boundary, y_boundary, linestyle='--', color='green', label='Perceptron Decision Boundary')
+plt.scatter(x0, y0, c='red', label='XOR=0', marker='o', s=100)
+plt.scatter(x1, y1, c='blue', label='XOR=1', marker='x', s=100)
+plt.xlabel('A')
+plt.ylabel('B')
+plt.title('Perceptron Decision Boundary')
+plt.legend(loc='upper right')
+plt.show()
+
+# 绘制反向传播神经网络的可视化
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([0, 1, 1, 0])
+model = MLPClassifier(hidden_layer_sizes=(4,), activation='relu', solver='lbfgs', random_state=1)
+model.fit(X, y)
+x_min, x_max = -0.5, 1.5
+y_min, y_max = -0.5, 1.5
+h = 0.01
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+plt.figure(figsize=(6, 4))
+plt.xlim(-0.5, 1.5)
+plt.ylim(-0.5, 1.5)
+plt.contourf(xx, yy, Z, cmap=plt.cm.RdYlBu, alpha=0.8)
+plt.scatter(X[y == 0][:, 0], X[y == 0][:, 1], c='red', label='XOR=0', marker='o', s=100)
+plt.scatter(X[y == 1][:, 0], X[y == 1][:, 1], c='blue', label='XOR=1', marker='x', s=100)
+plt.xlabel('A')
+plt.ylabel('B')
+plt.title('Backpropagation Neural Network')
+plt.legend(loc='upper right')
+plt.show()
